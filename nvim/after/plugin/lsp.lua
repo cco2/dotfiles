9baa54fd5b4c2ccc858ce51475ac0d0b4fe03ef2
cco2 <1665260+cco2@users.noreply.github.com>
2024-07-04 21:36:20 -0700

    more lsps

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -12,7 +12,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here
 	-- with the ones you want to install
-	ensure_installed = {'eslint', 'tsserver', 'rust_analyzer'},
+	ensure_installed = {'pylsp', 'pylyzer', 'pyright', 'eslint', 'tsserver', 'rust_analyzer'},
 	handlers = {
 		function(server_name)
 			require('lspconfig')[server_name].setup({})
